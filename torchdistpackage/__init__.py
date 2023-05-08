301bb55c2cae9ac27bf3a07f0287810da67a42d8
kimmishi <shidongxing@foxmail.com>
2023-05-08 14:10:48 +0800

    rename topo

--- torchdistpackage/__init__.py
+++ torchdistpackage/__init__.py
@@ -2,5 +2,5 @@ from .ddp.naive_ddp import NaiveDDP
 
 from .dist.launch_from_slurm import setup_distributed_slurm
 
-from .dist.topo import torch_parallel_context as tpc
-from .dist.topo import test_comm, is_using_pp
+from .dist.process_topo import torch_parallel_context as tpc
+from .dist.process_topo import test_comm, is_using_pp
